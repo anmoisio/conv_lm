0d9d6e2b82a95eca68d348569222e4e5584ddfe9
Anssi Moisio <anssi.moisio@aalto.fi>
2020-08-18 15:33:15 +0300

    helsinki ikuisesti movie ivector

---
+++ experiments/kaldi-am/helsinki-ivector/params.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+UTILS_DIR="${EXPT_SCRIPT_DIR}/utils"
+export PATH="${UTILS_DIR}:${PATH}"
+export TRAIN_CMD="${UTILS_DIR}/slurm.pl --mem 3G --time 1:00:00"
+export DECODE_CMD="${UTILS_DIR}/slurm.pl --mem 16G --time 1:00:00"
