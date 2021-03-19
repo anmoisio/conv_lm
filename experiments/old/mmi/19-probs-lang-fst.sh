22ae6c6178c7b1ea03fe52b6d6f699751d8ff499
Anssi Moisio <anssi.moisio@aalto.fi>
2021-03-19 10:29:52 +0200

    deleted old experiments

--- experiments/old/mmi/19-probs-lang-fst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-source ../../scripts/run-expt.sh "${0}"
-
-mkdir -p "${EXPT_WORK_DIR}/lang/train-test"
-cp -r "${EXPT_WORK_DIR}/lang/sp-test/"* "${EXPT_WORK_DIR}/lang/train-test/"
-rm -rf "${EXPT_WORK_DIR}/lang/train-test.tmp"
-cp "${EXPT_WORK_DIR}/lang/train-nosp/G."* "${EXPT_WORK_DIR}/lang/train-test/"
